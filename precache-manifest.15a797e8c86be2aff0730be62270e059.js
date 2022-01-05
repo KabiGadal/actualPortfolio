b442b0dce9907a7de9fd7cf33e731a29a2ae6482
Kabi <gadalkabi@gmail.com>
2022-01-05 19:17:09 +0530

    Updates

---
+++ precache-manifest.15a797e8c86be2aff0730be62270e059.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92a5eae7c43e6c8d7068d85912f75ec",
+    "url": "/actualPortfolio/index.html"
+  },
+  {
+    "revision": "2a174b7c8ec3bd2070dc",
+    "url": "/actualPortfolio/static/css/main.f8fb60f5.chunk.css"
+  },
+  {
+    "revision": "3ea9e20a31b164796ae3",
+    "url": "/actualPortfolio/static/js/2.15939b72.chunk.js"
+  },
+  {
+    "revision": "1736170c726943ca156780392fb7a06a",
+    "url": "/actualPortfolio/static/js/2.15939b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a174b7c8ec3bd2070dc",
+    "url": "/actualPortfolio/static/js/main.431f459f.chunk.js"
+  },
+  {
+    "revision": "d3f19f571d01f115cb33",
+    "url": "/actualPortfolio/static/js/runtime-main.230b52d6.js"
+  },
+  {
+    "revision": "244831e08af81d1777a904803d799f95",
+    "url": "/actualPortfolio/static/media/back.244831e0.png"
+  },
+  {
+    "revision": "0fa0030b8b569371a09fba3330d29b89",
+    "url": "/actualPortfolio/static/media/back2.0fa0030b.png"
+  },
+  {
+    "revision": "16e529ce8960b753d1e73a3e337bb46d",
+    "url": "/actualPortfolio/static/media/back3.16e529ce.png"
+  },
+  {
+    "revision": "cfc539ea2dd54b0e8e96f67a11f14c18",
+    "url": "/actualPortfolio/static/media/circle.cfc539ea.png"
+  },
+  {
+    "revision": "28ee7098db39c21adaccf692ec3b7c38",
+    "url": "/actualPortfolio/static/media/css.28ee7098.svg"
+  },
+  {
+    "revision": "9de32921fe1e3899c721d066d10da3ef",
+    "url": "/actualPortfolio/static/media/expressjs.9de32921.svg"
+  },
+  {
+    "revision": "1da2e4e6284edb442b26e873cb58e8a5",
+    "url": "/actualPortfolio/static/media/fassos_image.1da2e4e6.png"
+  },
+  {
+    "revision": "348811abdab0e51fa5f29cdd83aa9c22",
+    "url": "/actualPortfolio/static/media/fw10_199_Kabi_Gadal.348811ab.pdf"
+  },
+  {
+    "revision": "c0a6a539b4d8ec0e795df6d83991109b",
+    "url": "/actualPortfolio/static/media/javascript.c0a6a539.svg"
+  },
+  {
+    "revision": "cf553621a522b0353f6af38d19d6a250",
+    "url": "/actualPortfolio/static/media/myPhoto1.cf553621.jpeg"
+  },
+  {
+    "revision": "6c5a89ceb83cd851604103420d6abb76",
+    "url": "/actualPortfolio/static/media/nodeJs.6c5a89ce.svg"
+  },
+  {
+    "revision": "53be0ddadcf4de3de36c8d7be6221c8a",
+    "url": "/actualPortfolio/static/media/pepperfry_1.53be0dda.jfif"
+  },
+  {
+    "revision": "80c1018da4a9316f29897826b9844b26",
+    "url": "/actualPortfolio/static/media/purple_image.80c1018d.png"
+  },
+  {
+    "revision": "91b0953e9cf3dcc7522d4b1e41abb27d",
+    "url": "/actualPortfolio/static/media/reactjs.91b0953e.svg"
+  },
+  {
+    "revision": "15b11dc32e2da046f9e1705625a62916",
+    "url": "/actualPortfolio/static/media/redux.15b11dc3.svg"
+  },
+  {
+    "revision": "0ea071af727963c46ef6e9a446d75170",
+    "url": "/actualPortfolio/static/media/shopperstop_image.0ea071af.png"
+  }
+]);
